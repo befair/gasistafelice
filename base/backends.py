54da84aa341ef69c68dd317280dac8cc6810a328
Luca Ferroni <fero@jagom.org>
2011-03-10 05:53:59 +0100

    Added dummy backend for object permission checking

---
+++ base/backends.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.backends import ModelBackend
+
+class MyAuthBackend(ModelBackend):
+
+    supports_object_permissions = True
+    supports_anonymous_user = True
+    #supports_inactive_user = False
+
+    def has_perm(self, user, perm, obj=None):
+
+        if obj:
+            rv = obj.perm_check(user, perm)
+        else:
+            rv = super(ModelBackend, self).has_perm(user, perm)
+
+        return rv
