51f3a943a918bcb9dfc7ede201612e06f2e2003e
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2011-03-23 13:46:18 +0100

    Refined some order-related model definitions.
    * fixed how the `price` attribute is calculated for a
    `GASSupplierStock`
    * added more detailed comments
    * added some TODOs 
    * renamed some field attributes for improved readability

--- gas/models/order.py
+++ gas/models/order.py
@@ -3,7 +3,8 @@
 from django.db import models
 from django.utils.translation import ugettext, ugettext_lazy as _
 
-from gasistafelice.base.models import Place, GASMember, GAS
+from gasistafelice.base.models import Place 
+from gasistafelice.gas.models import GAS, GASMember, GASSupplierSolidalPact
 from gasistafelice.supplier.models import Supplier, SupplierStock
 from gasistafelice.gas.const import STATES_LIST
 
@@ -19,12 +20,15 @@ class GASSupplierStock(models.Model):
 
     gas = models.ForeignKey(GAS)
     supplier_stock = models.ForeignKey(SupplierStock)
+    # if a Product is available to GAS Members; policy is GAS-specific
+    enabled = models.BooleanField()    
     ## constraints on what a single GAS Member is able to order
     # minimun amount of Product units a GAS Member is able to order 
     order_minimum_amount = models.PositiveIntegerField(null=True, blank=True)
     # increment step (in Product units) after `order_minimum_amount`; 
     # useful when a Product ships in packages containing multiple units. 
     order_step = models.PositiveSmallIntegerField(null=True, blank=True)
+    
 
     @property
     def supplier(self):
@@ -33,8 +37,8 @@ class GASSupplierStock(models.Model):
     @property
     def price(self):
         # Product base price as updated by agreements contained in GASSupplierSolidalPact
-        price_percent_update = self.gas.supplier_set.get(supplier=self.supplier).price_percent_update
-        return self.supplier_stock.price*price_percent_update
+        price_percent_update = GASSupplierSolidalPact.objects.get(gas=self.gas, supplier=self.supplier).order_price_percent_update
+        return self.supplier_stock.price*(1 + price_percent_update)
 
 class GASSupplierOrder(models.Model):
     """An Order issued by a GAS to a Supplier.
@@ -48,20 +52,22 @@ class GASSupplierOrder(models.Model):
 
     gas = models.ForeignKey(GAS)
     supplier = models.ForeignKey(Supplier)
-    date_start = models.DateTimeField(help_text=_("when the order will be available"))
+    date_start = models.DateTimeField(help_text=_("when the order will be opened"))
     date_end = models.DateTimeField(help_text=_("when the order will be closed"))
     # Where and when delivery occurs
+    # TODO: factor out delivery information in a `DeliveryAppointment` model class
     delivery_date = models.DateTimeField(help_text=_("when the order will be delivered by supplier"))
-    delivery_place = models.ForeignKey('Place', related_name="delivery_for_order_set", help_text=_("where the order will be delivered by supplier"))
-    # How much has been delivered 
-    delivery_amount = models.PositiveIntegerField()
-    # Where and when withdraw occurs
-    withdraw_date = models.DateTimeField(help_text=_("when the order will be withdrawn by GAS members"))
-    withdraw_place = models.ForeignKey('Place', related_name="withdraw_for_order_set", help_text=_("where the order will be withdrawn by GAS members"))
+    delivery_place = models.ForeignKey('Place', related_name="deliveries", help_text=_("where the order will be delivered by supplier"))
+    # minimum economic amount for the GASSupplierOrder to be accepted by the Supplier  
+    order_minimum_amount = models.PositiveIntegerField(null=True, blank=True) # FIXME: should be a `CurrencyField` ?
+    # Where and when withdrawal occurs
+    # TODO: factor out withdrawal information in a `WithdrawalAppointment` model class
+    withdrawal_date = models.DateTimeField(help_text=_("when the order will be withdrawn by GAS members"))
+    withdrawal_place = models.ForeignKey('Place', related_name="withdrawals", help_text=_("where the order will be withdrawn by GAS members"))
 
     # STATUS is MANAGED BY WORKFLOWS APP: 
     # status = models.CharField(max_length=32, choices=STATES_LIST, help_text=_("order state"))
-    product_set = models.ManyToManyField(GASSupplierStock, help_text=_("products available for the order"), blank=True, through='GASSupplierOrderProduct')
+    products = models.ManyToManyField(GASSupplierStock, help_text=_("products available for the order"), blank=True, through='GASSupplierOrderProduct')
 
     def save(self):
         # If no product_set has been specified --> use all products bound to the supplier
@@ -78,14 +84,18 @@ class GASSupplierOrderProduct(models.Model):
 
     """
 
-    gassupplierorder = models.ForeignKey(GASSupplierOrder)
-    gassupplierstock = models.ForeignKey(GASSupplierStock)
-
+    gas_supplier_order = models.ForeignKey(GASSupplierOrder)
+    gas_supplier_stock = models.ForeignKey(GASSupplierStock)
+    # how many units of Product a GAS Member can request during this GASSupplierOrder
+    # useful for Products with a low availability
     maximum_amount = models.PositiveIntegerField(blank=True, default=0)
-
-    ordered_price = models.FloatField(blank=True)
+    # the price of the Product at the time the GASSupplierOrder was sent to the Supplier
+    ordered_price = models.FloatField(blank=True) # FIXME: should be a `CurrencyField` ?
+    # how many items were ordered (globally by the GAS)
     ordered_amount = models.PositiveIntegerField(blank=True)
-    delivered_price = models.FloatField(blank=True)
+    # the actual price of the Product (as resulting from the invoice)
+    delivered_price = models.FloatField(blank=True) # FIXME: should be a `CurrencyField` ?
+    # how many items were actually delivered by the Supplier 
     delivered_amount = models.PositiveIntegerField(blank=True)
     
 class GASMemberOrder(models.Model):
