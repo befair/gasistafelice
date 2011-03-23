bed97b2fd35c3c3e4565e8b18289a5373575ec79
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2011-03-23 19:10:38 +0100

    Misc minor modifications.
    * changed the field type for the homepage of a Supplier to `UrlField` 
    * removed permission checking code (`django-permissions` will be used
    for permission management on a per-object basis)
    * changed some attribute names for improved readability
    * changed some docstrings
    * misc comment modifications/cleanup

--- supplier/models.py
+++ supplier/models.py
@@ -1,28 +1,28 @@
-"""This model includes information about products, suppliers, producers.
+"""These models include information about Products, Suppliers, Producers.
 
-These are fundamental DES data to relies on. They represents market offering.
+These are fundamental DES data to rely on. They represent market offering.
 
-It relies on base model.
+Models here rely on base model classes.
 
-Definition: `Vocabolario - Fornitori <http://www.jagom.org/trac/REESGas/wiki/BozzaVocabolario#Fornitori>`__ (ita only)
+Definition: `Vocabolario - Fornitori <http://www.jagom.org/trac/REESGas/wiki/BozzaVocabolario#Fornitori>`__ (ITA only)
 """
 
 from django.db import models
 from django.utils.translation import ugettext, ugettext_lazy as _
 
 from gasistafelice.base import const
-from gasistafelice.base.models import Person
+from gasistafelice.base.models import Person, Place
 
 class Supplier(models.Model):
-    """The actor who has a stock of products to sell in the DES"""
+    """An actor having a stock of Products for sale to the DES."""
 
     name = models.CharField(max_length=128) 
-    address =  models.CharField(max_length=128, blank=True)
-    vat_number =  models.CharField(max_length=128)
-    home_page =  models.CharField(max_length=128, blank=True)
+    seat =  models.ForeignKey(Place)
+    vat_number =  models.CharField(max_length=128, unique=True) #TODO: perhaps a custom field needed here ? (for validation purposes)
+    website =  models.URLField(verify_exists=True, blank=True)
     referrers = models.ManyToManyField(Person, through="SupplierReferrer") 
     flavour = models.CharField(max_length=128, choices=const.SUPPLIER_FLAVOUR_LIST, default=const.SUPPLIER_FLAVOUR_LIST[0][0])
-    cert_set = models.ManyToManyField('Certification')
+    certifications = models.ManyToManyField('Certification')
 
     # the set of products provided by this Supplier to every GAS
     @property
@@ -33,15 +33,14 @@ class Supplier(models.Model):
         return self.name
 
 class SupplierReferrer(models.Model):
-
     supplier = models.ForeignKey(Supplier)
     person = models.ForeignKey(Person)
-    job_role = models.CharField(max_length=256, blank=True)
+    job_title = models.CharField(max_length=256, blank=True)
     job_description = models.TextField(blank=True)
 
     
 class Certification(models.Model):
-    name = models.CharField(max_length=128) 
+    name = models.CharField(max_length=128, unique=True) 
     description = models.TextField(blank=True)
 
     def __unicode__(self):
@@ -50,7 +49,6 @@ class Certification(models.Model):
 class ProductCategory(models.Model):
     # Proposal: the name is in the form MAINCATEGORY::SUBCATEGORY
     # like sourceforge categories
-    # Proposta usare solo il nome CATEGORIA::SOTTOCATEGORIA
     name = models.CharField(max_length=128, unique=True, blank=False)
     description = models.TextField(blank=True)
 
@@ -58,11 +56,12 @@ class ProductCategory(models.Model):
         return self.name
 
 class ProductMU(models.Model):
-    """Measurement unit for the product.
-
-    Implemented as a separated entity like GasDotto software.
-    Each supplier referrer has to be able to create its own measurement units.
+    """Measurement unit for a Product.
+         
     """
+    # Implemented as a separated entity like GasDotto software.
+    # Each SupplierReferrer has to be able to create its own measurement units.
+    
     name = models.CharField(max_length=32, unique=True, blank=False)
     description = models.TextField(blank=True)
 
@@ -71,45 +70,35 @@ class ProductMU(models.Model):
 
 class Product(models.Model):
 
-    uuid = models.CharField(max_length=128, unique=True, blank=True, null=True)
+    uuid = models.CharField(max_length=128, unique=True, blank=True, null=True) # if empty, should be programmatically set at DB save time
     producer = models.ForeignKey(Supplier)
     category = models.ForeignKey(ProductCategory)
     mu = models.ForeignKey(ProductMU)
     name = models.CharField(max_length=128)
     description = models.TextField(blank=True)
     
-    def permission_check(self, user, perm):
-
-        if perm == const.SUPPLIER_REFERRER:
-            rv = user in self.referrers.values_list('user')
-        elif perm == const.GAS_REFERRER_TECH:
-            rv = True
-#        elif perm == const.STACIPPA:
-#            rv = False
-#            for i in self.referrers:
-#                if i.name == "stacippa":
-#                    rv = True
-#                    break
-        else:
-            # We checked all available permissions...
-            raise PermissionDoesNotExist
-        return rv
-
     @property
     def referrers(self):
         return self.producer.referrers.all()
 
 class SupplierStock(models.Model):
-    """Product that a Supplier offers in the DES marketplace"""
+    """A Product that a Supplier offers in the DES marketplace.
+        
+       Includes price, order constraints and availability information.          
+    """
 
     supplier = models.ForeignKey(Supplier)
     product = models.ForeignKey(Product)
-    price = models.FloatField()
+    price = models.FloatField() # FIXME: should be a `CurrencyField` ?
     amount_available = models.PositiveIntegerField(default=const.ALWAYS_AVAILABLE)
-    # Amount and step are referred to the supplier
+    ## constraints posed by the Supplier on orders issued by *every* GAS
+    # minimum amount of Product units a GAS is able to order 
     order_minimum_amount = models.PositiveIntegerField(null=True, blank=True)
+    # increment step (in Product units) for amounts exceeding minimum; 
+    # useful when a Product ships in packages containing multiple units.
     order_step = models.PositiveSmallIntegerField(null=True, blank=True)
-
+    # how the Product will be delivered
+    delivery_terms = models.TextField(null=True, blank=True) #FIXME: find a better name for this attribute 
     @property
     def producer(self):
         return self.product.producer
