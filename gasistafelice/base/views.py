694e8bf4b19d4944037186647cebf94df11cd28c
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2011-04-12 12:10:50 +0200

    Removed draft-level code.

--- gasistafelice/base/views.py
+++ gasistafelice/base/views.py
@@ -1,7 +1,1 @@
 # Create your views here.
-
-def product_update(request, product_id):
-    p = get_object_or_404(pk=product_id)
-    if (request.user.has_perm(const.SUPPLIER_REFERRER, p))
-        context = {}
-    return 
