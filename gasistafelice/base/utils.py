97940fb93aa7a6285ee96f611d57335de5295f0e
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2011-04-12 12:10:12 +0200

    Added a general-purpose utility function.

---
+++ gasistafelice/base/utils.py
@@ -0,0 +1,16 @@
+from django.contrib.contenttypes.models import ContentType
+
+def get_ctype_from_model_label(label):
+    """
+    This helper function takes a model identifier (a string of the form
+    'app_label.model_name', where `app_label` is as in Django docs and `model_name`
+    is the model class' name, and return the ContentType instance associated with 
+    the model class. If the label is malformed or there is no model with that label, 
+    raise a `TypeError`.     
+    """
+    try:
+        (app_label, model_name) = label.split('.')
+        ctype = ContentType.objects.get(app_label=app_label, model=model_name)
+        return ctype        
+    except:
+        raise TypeError("There is no model corresponding to the given label.") 
