f5099f1b27a093da020e50c197a07e7b0dde3bbd
Dominique THUAL <dom_thual@yahoo.fr>
2011-04-16 22:51:01 +0200

    internationalization localization

--- gasistafelice/gas/models/base.py
+++ gasistafelice/gas/models/base.py
@@ -50,10 +50,10 @@ class GAS(PermissionResource, models.Model):
 
     active = models.BooleanField()
     birthday = models.DateField()
-    vat =  models.CharField(max_length=11, null=True, blank=True, help_text=_("Partita IVA"))	
-    fiscal_code =  models.CharField(max_length=16, null=True, blank=False, help_text=_("Codice fiscale"))	
+    vat =  models.CharField(max_length=11, null=True, blank=True, help_text=_("VAT number"))
+    fiscal_code =  models.CharField(max_length=16, null=True, blank=False, help_text=_("Fiscal code"))
     email_gas = models.EmailField()
-    email_referrer = models.EmailField(null=True, blank=True, help_text=_("Email responsabili"))
+    email_referrer = models.EmailField(null=True, blank=True, help_text=_("Email coordinator"))
     phone = models.CharField(max_length=50, null=True, blank=True)	
     website = models.URLField(verify_exists=True, null=True, blank=True) 
     #TODO: gallery album
@@ -222,19 +222,17 @@ class GASSupplierSolidalPact(PermissionResource, models.Model):
               )     
         return rv
      
-<<<<<<< HEAD
-=======
     def elabore_report(self):
         #TODO return report like pdf format. Report has to be signed-firmed by partners
         return "" 
 
 #TODO: put in base and import
 class AbstractClass(models.Model):
-    created_at=models.DateField(_"Created at")
-    created_by=models.ForeignKey(User, db_column="created_by", related_name=_"poll_user_created_by")
-    updated_at=models.DateTimeField("Updated at")
-    updated_by=models.ForeignKey(User, db_column="updated_by", null=True, related_name=_"poll_user_updated_by")
->>>>>>> init GAS models configuration
+    created_at=models.DateField(_("Created at"))
+    created_by=models.ForeignKey(User, db_column="created_by", related_name=_("user_created_by"))
+    updated_at=models.DateTimeField(_("Updated at"))
+    updated_by=models.ForeignKey(User, db_column="updated_by", null=True, related_name=_("user_updated_by"))
+
     class Meta:
         app_label = 'gas'
 
