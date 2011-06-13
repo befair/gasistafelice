e4c226bb9a5a941b505a337f7b8a39e4113acbd3
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2011-06-13 20:18:04 +0200

    Added methods for missing roles in `GasMemberManager`.

--- gasistafelice/gas/managers.py
+++ gasistafelice/gas/managers.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from gasistafelice.auth.models import ParamRole
+
 class GASMembersManager(models.Manager):
 
     def have_role(self, parametric_role):
@@ -11,20 +13,60 @@ class GASMembersManager(models.Manager):
         referrer_as_users = User.objects.filter(principal_param_role_relation__in=parametric_roles)
         return self.get_query_set().filter(person__user_in=referrer_as_users)
 
+    def gas_referrers(self, **resource_kw):
+        """
+        Return a QuerySet containing all GAS members who have been assigned the 'GAS Referrer'
+        role for the GAS they belong to.    
+        """
+        parametric_roles = ParamRole.objects.gas_tech_referrers(**resource_kw)
+        return self.have_roles(parametric_roles)
+
+
     def tech_referrers(self, **resource_kw):
+        """
+        Return a QuerySet containing all GAS members who have been assigned the 'Tech Referrer'
+        role for the GAS they belong to.    
+        """
         parametric_roles = ParamRole.objects.gas_tech_referrers(**resource_kw)
         return self.have_roles(parametric_roles)
 
     def cash_referrers(self):
-        parametric_roles = ParamRole.objects.gas_cash_referrers(**resource_kw)
+        """
+        Return a QuerySet containing all GAS members who have been assigned the 'Cash Referrer'
+        role for the GAS they belong to.    
+        """
+        parametric_roles = ParamRole.objects.gas_cash_referrers()
         return self.have_roles(parametric_roles)
 
     def supplier_referrers(self):
-        parametric_roles = ParamRole.objects.gas_supplier_referrers(**resource_kw)
+        """
+        Return a QuerySet containing all GAS members who have been assigned the 'Supplier Referrer'
+        role for the GAS they belong to.    
+        """
+        parametric_roles = ParamRole.objects.gas_supplier_referrers()
+        return self.have_roles(parametric_roles)
+
+    def order_referrers(self):
+        """
+        Return a QuerySet containing all GAS members who have been assigned the 'Order Referrer'
+        role for the GAS they belong to.    
+        """
+        parametric_roles = ParamRole.objects.gas_supplier_referrers()
         return self.have_roles(parametric_roles)
 
     def delivery_referrers(self):
-        parametric_roles = ParamRole.objects.gas_delivery_referrers(**resource_kw)
+        """
+        Return a QuerySet containing all GAS members who have been assigned the 'Delivery Referrer'
+        role for the GAS they belong to.    
+        """
+        parametric_roles = ParamRole.objects.gas_delivery_referrers()
         return self.have_roles(parametric_roles)
 
+    def withdrawal_referrers(self):
+        """
+        Return a QuerySet containing all GAS members who have been assigned the 'Withdrawal Referrer'
+        role for the GAS they belong to.    
+        """
+        parametric_roles = ParamRole.objects.gas_delivery_referrers()
+        return self.have_roles(parametric_roles)
 
