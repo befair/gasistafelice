5cc3bfa9f0deedde1c6a0bb1194a8028e38981d6
Luca Ferroni <luca@befair.it>
2011-06-15 19:39:21 +0200

    Add GASConfig instance when creating a GAS instance
    ... and a GASSupplierStock when we are adding a GASSupplierSolidalPact
    overriding save() when modification is model-contained.
    
    Use this convention for basic model modifications. Signals should be used when we extend a model.
    http://stackoverflow.com/questions/170337/django-signals-vs-overriding-save-method
    
    I agree with the answer answered Oct 5 '08 at 8:38

--- gasistafelice/gas/models/__init__.py
+++ gasistafelice/gas/models/__init__.py
@@ -8,13 +8,3 @@ Definition: `Vocabolario - GAS <http://www.jagom.org/trac/REESGas/wiki/BozzaVoca
 from gasistafelice.gas.models.base import GAS, GASMember, GASSupplierStock, GASSupplierSolidalPact, GASConfig
 from gasistafelice.gas.models.order import GASSupplierOrder, GASSupplierOrderProduct, GASMemberOrder, Delivery, Withdrawal
 
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-@receiver(post_save, sender=GASSupplierSolidalPact)
-def auto_select_all_products_if_configured(sender, instance, created, **kwargs):
-
-    if created and instance.gas.config.auto_select_all_products:
-        for p in self.supplier.supplierstock_set.all():
-            p.gassupplierstock_set.add(gas=self.gas)
-
