daf437a841b9127a84d57cd5701ce2dffe699234
Luca Ferroni <luca@befair.it>
2011-06-15 21:11:40 +0200

    Now Fixed GASMemberManager.have_role

--- gasistafelice/gas/managers.py
+++ gasistafelice/gas/managers.py
@@ -4,10 +4,13 @@ from django.contrib.auth.models import User
 class GASMembersManager(models.Manager):
 
     def have_role(self, parametric_role):
-        return self.get_query_set().filter(person__user_principal_param_role_set__in=[parametric_role])
+        return self.get_query_set().filter(person__user_principal_param_role_set__in=[parametric_role], gas=parametric_role.gas)
 
     def have_roles(self, parametric_roles):
-        return self.get_query_set().filter(person__user_principal_param_role_set__in=parametric_roles)
+        qs = self.get_query_set().none()
+        for p in parametric_roles:
+            qs = qs | self.have_role(p)
+        return qs
 
     def tech_referrers(self, **resource_kw):
         parametric_roles = ParamRole.objects.gas_tech_referrers(**resource_kw)
