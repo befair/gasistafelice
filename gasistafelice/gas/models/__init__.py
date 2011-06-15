44ac5d7fa5c272834a635246d17f111ce3c90bee
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2011-06-15 19:21:34 +0200

    Added a `try` block to cater for non-existing GAS configuration.

--- gasistafelice/gas/models/__init__.py
+++ gasistafelice/gas/models/__init__.py
@@ -4,17 +4,20 @@ They rely on base models and Supplier-related ones to get Product and Stock info
 
 Definition: `Vocabolario - GAS <http://www.jagom.org/trac/REESGas/wiki/BozzaVocabolario#GAS>`__ (ITA only)
 """
+ 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 from gasistafelice.gas.models.base import GAS, GASMember, GASSupplierStock, GASSupplierSolidalPact, GASConfig
 from gasistafelice.gas.models.order import GASSupplierOrder, GASSupplierOrderProduct, GASMemberOrder, Delivery, Withdrawal
 
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 @receiver(post_save, sender=GASSupplierSolidalPact)
 def auto_select_all_products_if_configured(sender, instance, created, **kwargs):
-
-    if created and instance.gas.config.auto_select_all_products:
-        for p in self.supplier.supplierstock_set.all():
-            p.gassupplierstock_set.add(gas=self.gas)
-
+    try:
+        if created and instance.gas.config.auto_select_all_products:
+            for p in instance.supplier.supplierstock_set.all():
+                p.gassupplierstock_set.add(gas=instance.gas)
+    except GASConfig.DoesNotExist: # if GAS configuration is not defined, do nothing
+        pass
+    
