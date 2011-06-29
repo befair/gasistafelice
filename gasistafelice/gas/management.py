b1bb08a8ee246774b43e521e8f754cdcc88c418b
Luca Ferroni <luca@befair.it>
2011-06-29 18:31:29 +0200

    Fix in post_syncdb workflow registration

--- gasistafelice/gas/management.py
+++ gasistafelice/gas/management.py
@@ -4,7 +4,7 @@ from gasistafelice.gas.workflow_data import workflow_dict
 
 def init_workflows(app, created_models, verbosity, **kwargs):
     app_label = app.__name__.split('.')[-2]
-    if app_label == 'workflows' and created_models: # `worklows` app was syncronized for the first time
+    if app_label == 'workflows' and "Workflow" in created_models: # `worklows` app was syncronized for the first time
         # now that all necessary tables are in the DB, we can register our workflows
         for name, w in workflow_dict.items():
             w.register_workflow()
