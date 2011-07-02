819182ec28d2eb96dba1bf9acff7601dc4d6f9dd
domthu <dom_thual@yahoo.fr>
2011-07-03 00:57:15 +0200

    rebase to master

--- gasistafelice/gas/models/base.py
+++ gasistafelice/gas/models/base.py
@@ -324,10 +324,8 @@ class GASMember(models.Model, PermissionResource):
     def setup_roles(self):
         # automatically add a new GASMember to the `GAS_MEMBER` Role
         user = self.person.user
-        #COMMENT: issue #3 TypeError: The principal must be either a User instance or a Group instance.
-        if user is None:
-           return ""
-        #TODO: fixtures create user foreach person
+        #if user is None:
+        #    return ""
         role = register_parametric_role(name=GAS_MEMBER, gas=self.gas)
         #COMMENT: issue #3 TypeError: The principal must be either a User instance or a Group instance.
         #TODO: fixtures create user foreach person
