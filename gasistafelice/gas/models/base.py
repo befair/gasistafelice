d2b02df7696cf1fd7815501c0f408887b1369d28
Luca Ferroni <luca@befair.it>
2011-07-06 23:07:39 +0200

    Fix circular import and fix GAS save with no DES

--- gasistafelice/gas/models/base.py
+++ gasistafelice/gas/models/base.py
@@ -116,6 +116,13 @@ class GAS(models.Model, PermissionResource):
 
         if not self.id_in_des:
             self.id_in_des = self.name[:3]
+            while True:
+                try:
+                    GAS.objects.get(id_in_des=self.id_in_des)
+                    self.id_in_des = self.id_in_des[:2] + chr(ord(self.id_in_des[2]) + 1)
+                except: #DoesNotExist or MultipleObjectsReturned
+                    break
+                    
         self.id_in_des = self.id_in_des.upper()
 
         if not self.pk:
@@ -126,7 +133,9 @@ class GAS(models.Model, PermissionResource):
 
         # This should never happen, but is it reasonable
         # that an installation has only one DES
-        if not self.des:
+        try:
+            self.des
+        except DES.DoesNotExist:
             if DES.objects.count() > 1:
                 raise AttributeError(_("You have to bind GAS %s to a DES") % self.name)
             else:
