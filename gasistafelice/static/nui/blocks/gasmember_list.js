09d93d3c79727af98c34b5893bc1ce85c2828891
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2011-07-07 12:50:09 +0200

    Added list blocks for GAS members and solidal pacts + minor fixes
    * Added a list block for GAS members (view + JS);
    * Added a list block for solidal pacts (view + JS);
    * in `gas_list.py`: in `BLOCK_VALID_RESOURCE_TYPES`: `user` resource
    should actually be `person` (according to resource definition in
    `globals.py`);
    * Renamed `suppliers.py` to `supplier_list.py`, according to naming
    conventions for list blocks.
    * Renamed `suppliers.js` to `supplier_list.js`, according to naming
    conventions for list blocks.

---
+++ gasistafelice/static/nui/blocks/gasmember_list.js
@@ -0,0 +1,2 @@
+jQuery.REGISTER_BLOCK_UPDATE_HANDLER('gasmember_list', jQuery.resource_list_block_update);
+/* TODO jQuery.BLOCK_REGISTER_DISPLAY_DEFAULT_BY_NAME = { 'gas_listmember' : 'resource_list_as_icons' } */
--- gasistafelice/static/nui/blocks/suppliers.js
+++
@@ -1,4 +0,0 @@
-
-
-jQuery.REGISTER_BLOCK_UPDATE_HANDLER('suppliers', jQuery.resource_list_block_update);
-/* TODO jQuery.BLOCK_REGISTER_DISPLAY_DEFAULT_BY_NAME = { 'suppliers' : 'resource_list_as_icons' } */
