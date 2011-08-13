4131fab81e8e46ab1a0775bddf16bf1e7616b947
Luca Ferroni <luca@befair.it>
2011-08-13 13:14:46 +0200

    Closed orders block
    * rest/views/blocks/closed_orders.py: inherit directly from open orders block!

---
+++ gasistafelice/rest/views/blocks/closed_orders.py
@@ -0,0 +1,20 @@
+from django.utils.translation import ugettext as _, ugettext_lazy as _lazy
+from gasistafelice.rest.views.blocks.open_orders import Block as OpenOrdersBlock
+
+#------------------------------------------------------------------------------#
+#                                                                              #
+#------------------------------------------------------------------------------#
+
+class Block(OpenOrdersBlock):
+
+    BLOCK_NAME = "closed_orders"
+    BLOCK_DESCRIPTION = _("Closed orders")
+    BLOCK_VALID_RESOURCE_TYPES = ["site", "supplier", "gas"] 
+
+    def _get_resource_list(self, request):
+        return request.resource.orders.closed()
+
+    def _get_user_actions(self, request):
+
+        return []
+
---
+++ gasistafelice/static/nui/blocks/closed_orders.js
@@ -0,0 +1,2 @@
+
+jQuery.BLOCKS["closed_orders"] = new jQuery.UIBlockWithList("closed_orders", "list");
