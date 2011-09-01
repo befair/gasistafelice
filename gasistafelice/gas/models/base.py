b516002b002c18542a0dda03c173e63cdc22ea6a
domthu <dom_thual@yahoo.fr>
2011-09-01 16:03:05 +0200

    Display_fields for GASMember

--- gasistafelice/gas/models/base.py
+++ gasistafelice/gas/models/base.py
@@ -29,7 +29,7 @@ from gasistafelice.exceptions import NoSenseException
 
 from decimal import Decimal
 
-from django.db.models.signals import pre_save, post_save
+from django.db.models.signals import post_save
 
 class GAS(models.Model, PermissionResource):
 
@@ -544,7 +544,7 @@ class GASMember(models.Model, PermissionResource):
 
     @property
     def city(self):
-        return self.person.city 
+        return self.person.city
 
     @property
     def economic_state(self):
@@ -854,21 +854,6 @@ class GASSupplierSolidalPact(models.Model, PermissionResource):
 
 
 #-------------------------------------------------------------------------------
-def setup_attribute_data(sender, instance, created, **kwargs):
-    """
-    Setup proper attribute data before a model instance is saved to the DB for the first time.
-    This function just calls the `setup_attribute_data()` instance method of the sender model class (if defined);
-    actual role-creation/setup logic is encapsulated there.
-    """
-    if created: # Automatic attribute-setup should happen only at instance-creation time 
-        try:
-            # `instance` is the model instance that has just been created
-            instance.setup_attribute_data()
-                                                
-        except AttributeError:
-            # sender model doesn't specify any data-related setup operations, so just ignore the signal
-            pass
-
 def setup_data(sender, instance, created, **kwargs):
     """
     Setup proper data after a model instance is saved to the DB for the first time.
