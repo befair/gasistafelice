67a2008212c556773caf89a2b2b487b4ba7be7cf
Luca Ferroni <luca@befair.it>
2011-10-19 01:07:04 +0200

    Added logging stuff

--- gasistafelice/default_settings.py
+++ gasistafelice/default_settings.py
@@ -130,6 +130,69 @@ FIXTURE_DIRS = (
     os.path.join(PROJECT_ROOT, 'fixtures/supplier/'),
 )
 
+LOG_FILE = os.path.join(PROJECT_ROOT, 'gf.log')
+LOG_FILE_DEBUG = os.path.join(PROJECT_ROOT, 'gf_debug.log')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
+        },
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'logfile':{
+            'level':'INFO',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': LOG_FILE,
+            'maxBytes': 1048576,
+            'backupCount' : 5,
+            'formatter': 'simple'
+        },
+        'logfile_debug':{
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': LOG_FILE_DEBUG,
+            'maxBytes': 1048576,
+            'backupCount' : 10,
+            'formatter': 'verbose'
+        },
+#        'mail_admins': {
+#            'level': 'ERROR',
+#            'class': 'django.utils.log.AdminEmailHandler',
+#        }
+    },
+    'loggers': {
+        'django': {
+            'handlers':['null'],
+            'propagate': True,
+            'level':'INFO',
+        },
+        'django.request': {
+            'handlers': ['logfile'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'gasistafelice': {
+            'handlers': ['console', 'logfile', 'logfile_debug'],
+            'level': 'DEBUG',
+        }
+    }
+}
+
 THEME = "milky"
 AUTH_PROFILE_MODULE = 'users.UserProfile'
 URL_PREFIX = "gasistafelice/"
