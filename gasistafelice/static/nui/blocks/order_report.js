d045f515602ce2e54a2b6670df4c2f720e0f55c2
domthu <dom_thual@yahoo.fr>
2011-10-31 02:29:16 +0100

    Set resource pattern for grid order_report
    - order_report: product
    Can close ticket 177

--- gasistafelice/static/nui/blocks/order_report.js
+++ gasistafelice/static/nui/blocks/order_report.js
@@ -43,6 +43,19 @@ jQuery.UIBlockOrderReport = jQuery.UIBlockWithList.extend({
                     "sInfoEmpty": gettext("Showing 0 to 0 of 0 records"),
                     "sInfoFiltered": gettext("(filtered from _MAX_ total records)")
                 },
+                "fnRowCallback": function(nRow, aaData, iDisplayIndex, iDisplayIndexFull) {
+                    try {
+                        var url = aaData[9];
+                        if (url != undefined) {
+                            var _name = aaData[1];
+                            res = new jQuery.Resource(url, _name);
+                            $(nRow.cells[1]).html( res.render() );
+                        }
+                    }
+                    catch(e){alert(e.message);
+                    }
+                    return nRow
+                } ,
                 "fnFooterCallback": function ( nRow, aaData, iStart, iEnd, aiDisplay ) {
 
                     var iTotal = 0;
