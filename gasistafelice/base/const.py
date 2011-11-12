c9cc6f81bdce8be05a8ce7d87643ebd3466bbceb
Luca Ferroni <luca@befair.it>
2011-11-12 10:50:17 +0100

    Removed WWW from consts

--- gasistafelice/base/const.py
+++ gasistafelice/base/const.py
@@ -12,12 +12,14 @@ ALWAYS_AVAILABLE = 1000000000
 PHONE = 'PHONE'
 EMAIL = 'EMAIL'
 FAX = 'FAX'
+#WWW = 'WWW'
 
 CONTACT_CHOICES = [
     (PHONE, _('PHONE')),
     (EMAIL, _('EMAIL')),
     (FAX, _('FAX')),
 ]
+#    (WWW, _('WWW')),
 
 DAY_CHOICES = [
     ('MONDAY', _('Monday')),
