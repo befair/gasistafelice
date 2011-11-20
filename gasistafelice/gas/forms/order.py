ce4db4205d6b1aba4cdd1e4d61db4835aa55c5c1
domthu <dom_thual@yahoo.fr>
2011-11-20 17:27:10 +0100

    EditOrderForm

--- gasistafelice/gas/forms/order.py
+++ gasistafelice/gas/forms/order.py
@@ -74,7 +74,7 @@ class BaseOrderForm(forms.ModelForm):
 
     delivery_datetime = forms.SplitDateTimeField(required=False, label=_('Delivery on/at'), widget=GFSplitDateTimeWidget)
 
-    delivery_referrer = forms.ModelChoiceField(queryset=Person.objects.none(), required=False)
+    delivery_referrer = forms.ModelChoiceField(queryset=Person.objects.none(), required=True)
     withdrawal_referrer = forms.ModelChoiceField(queryset=Person.objects.none(), required=False)
 
     def __init__(self, request, *args, **kw):
@@ -276,13 +276,23 @@ class AddOrderForm(BaseOrderForm):
 class EditOrderForm(BaseOrderForm):
 
     log.debug("EditOrderForm")
-    delivery_terms = forms.CharField(label=_('Delivery terms'), required=False, widget=widgets.Textarea)
+    #delivery_terms = forms.CharField(label=_('Delivery terms'), required=False, widget=widgets.Textarea)
 
     def __init__(self, request, *args, **kw):
 
         super(EditOrderForm, self).__init__(request, *args, **kw)
 
-        #TODO retrieve data from existing data onto database
+        #SOLIDAL PACT
+        pact = request.resource.pact
+        delivery = request.resource.delivery
+        refs = request.resource.delivery_referrer_persons
+        refs = request.resource.delivery_referrer_persons
+        if refs:
+            self.fields['delivery_referrer'].initial = refs[0]
+        if request.resource.datetime_end:
+            self.fields['datetime_end'].initial = request.resource.datetime_end
+        if delivery and delivery.date:
+            self.fields['delivery_datetime'].initial = delivery.date
 
     def save(self):
 
@@ -300,12 +310,13 @@ class EditOrderForm(BaseOrderForm):
         model = GASSupplierOrder
         fields = ['datetime_start', 'datetime_end']
 
-        gf_fieldsets = [(None, { 
-            'fields' : [ ('datetime_start', 'datetime_end'), 
-                         ('delivery_referrer', 'withdrawal_referrer')
-                        ,'delivery_terms'
-            ] 
+        gf_fieldsets = [(None, {
+            'fields' : [ ('datetime_start', 'datetime_end'),
+                       ('delivery_datetime', 'delivery_referrer')
+            ]
         })]
+                       #,'withdrawal_referrer'
+                       #,'delivery_terms'
 
 def form_class_factory_for_request(request, base):
     """Return appropriate form class basing on GAS configuration
