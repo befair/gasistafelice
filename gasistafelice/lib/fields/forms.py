7bb8933efc3f5baf3186b0d84e61912e42e190b8
Luca Ferroni <luca@befair.it>
2011-12-04 23:08:22 +0100

    CurrencyField.clean() supports None values

--- gasistafelice/lib/fields/forms.py
+++ gasistafelice/lib/fields/forms.py
@@ -10,14 +10,14 @@ class CurrencyField(DecimalField):
         super(CurrencyField, self).__init__(*args, **kw)
 
     def clean(self, value):
-        dot_pos = value.rfind('.')
-        comma_pos = value.rfind(',')
 
-        if comma_pos > dot_pos:
-            new_value = value[:comma_pos] + '.' + value[comma_pos+1:]
-        else:
-            new_value = value
+        if value is not None:
+            dot_pos = value.rfind('.')
+            comma_pos = value.rfind(',')
 
-        return super(CurrencyField, self).clean(new_value)
+            if comma_pos > dot_pos:
+                value = value[:comma_pos] + '.' + value[comma_pos+1:]
+
+        return super(CurrencyField, self).clean(value)
 
 
