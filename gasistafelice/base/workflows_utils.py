c8dec30d5c947242e0b8aef2d8e97ad1d07bd69f
Luca Ferroni <luca@befair.it>
2011-12-05 14:24:48 +0100

    Fixed: order update state -> allowed_transition retrieval!
    In turn it displays action buttons in order details block

--- gasistafelice/base/workflows_utils.py
+++ gasistafelice/base/workflows_utils.py
@@ -37,6 +37,7 @@ def get_allowed_transitions(obj, user):
     for pr in param_roles:
         if user in pr.get_users():
             rv = workflows.utils.get_allowed_transitions(obj, user)
+            break
         else:
             rv = []
 
