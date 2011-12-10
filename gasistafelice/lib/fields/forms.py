b1ebd7342c2cd37c341c8a8812e3a501b91d3f57
Luca Ferroni <luca@befair.it>
2011-12-10 19:02:45 +0100

    Tryied PyPrettyDecimal for forms.CurrencyField

--- gasistafelice/lib/fields/forms.py
+++ gasistafelice/lib/fields/forms.py
@@ -1,5 +1,32 @@
 
 from django.forms import DecimalField
+from lib.fields.models import PyPrettyDecimal
+
+from decimal import Decimal, DecimalException
+from django.core.exceptions import ValidationError
+from django.core import validators
+from django.utils import formats
+from django.utils.encoding import smart_str
+
+class PrettyDecimalField(DecimalField):
+
+    def to_python(self, value):
+        """
+        Validates that the input is a decimal number. Returns a Decimal
+        instance. Returns None for empty values. Ensures that there are no more
+        than max_digits in the number, and no more than decimal_places digits
+        after the decimal point.
+        """
+        if value in validators.EMPTY_VALUES:
+            return None
+        if self.localize:
+            value = formats.sanitize_separators(value)
+        value = smart_str(value).strip()
+        try:
+            value = PyPrettyDecimal(value)
+        except DecimalException:
+            raise ValidationError(self.error_messages['invalid'])
+        return value
 
 class CurrencyField(DecimalField):
     """CurrencyField that accept ',' and '.' as decimal separator"""
