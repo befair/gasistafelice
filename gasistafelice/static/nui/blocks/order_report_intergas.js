bd2399ffda4e3bc7b3f2c250af90441a73970769
THUAL Dominique <dom_thual@yahoo.fr>
2012-06-30 00:36:26 +0200

    remove paging in order_report
    some problem for filtering order_report
    filtering go in trouble --> invalid icontains?
    
    Add old order_report_intergas files for intergas report

--- gasistafelice/static/nui/blocks/order_report_intergas.js
+++ gasistafelice/static/nui/blocks/order_report_intergas.js
@@ -1,9 +1,9 @@
 
-jQuery.UIBlockOrderReport = jQuery.UIBlockWithList.extend({
+jQuery.UIBlockOrderInterGAS = jQuery.UIBlockWithList.extend({
 
     init: function() {
-        this._super("order_report", "table");
-        this.submit_name = "Togli prodotti dall'ordine (elimina ordine gasista)";
+        this._super("order_report_intergas", "table");
+        this.submit_name = "Togli prodotti in tutti GAS";
     },
         //this.active_view = "edit_multiple";
         //this.default_view = this.active_view;
@@ -18,26 +18,23 @@ jQuery.UIBlockOrderReport = jQuery.UIBlockWithList.extend({
 
     rendering_table_post_load_handler: function() {
 
-        var block_obj = this;
         // Init dataTables
         var iTot = 7;
-                //'sPaginationType': 'full_numbers',
         var oTable = this.block_el.find('.dataTable').dataTable({
-                'bPaginate': false, 
+                'sPaginationType': 'full_numbers', 
                 "bServerSide": true,
                 "bStateSave": true,
                 "sAjaxSource": this.get_data_source(),
-                "aaSorting": [[1,"asc"]],
                 "aoColumns": [
-                    { "sWidth": "5%", "bSearchable":true},
-                    { "sWidth": "30%", "bSearchable":true},
+                    { "sWidth": "5%"},
+                    { "sWidth": "30%"},
                     { "sWidth": "10%", "sType": "currency", "sClass": "taright", "bSearchable" : false},
-                    { "sWidth": "5%", "bSortable" : false, "bSearchable" : false},
+                    { "sWidth": "5%", "bSortable" : false, "bSortable" : false, "bSearchable" : false},
                     { "sWidth": "10%", "bSortable" : false, "sClass": "taright", "bSearchable" : false},
                     { "sWidth": "5%", "bSortable" : false, "bSearchable" : false},
                     { "sWidth": "15%", "bSortable" : false, "sClass": "taright", "bSearchable" : false},
                     { "sWidth": "15%", "bSortable" : false, "sClass": "taright", "bSearchable" : false},
-                    { "sWidth": "5%", "bSearchable" : false}
+                    { "sWidth": "5%"},
                 ],
                 "oLanguage": {
                     "sLengthMenu": gettext("Display _MENU_ records per page"),
@@ -49,7 +46,7 @@ jQuery.UIBlockOrderReport = jQuery.UIBlockWithList.extend({
                 "fnRowCallback": function(nRow, aaData, iDisplayIndex, iDisplayIndexFull) {
                     try {
                         var url = aaData[9];
-                        if (url !== undefined) {
+                        if (url != undefined) {
                             var _name = aaData[1];
                             res = new jQuery.Resource(url, _name);
                             $(nRow.cells[1]).html( res.render() );
@@ -57,23 +54,19 @@ jQuery.UIBlockOrderReport = jQuery.UIBlockWithList.extend({
                     }
                     catch(e){alert(e.message);
                     }
-                    return nRow;
+                    return nRow
                 } ,
                 "fnFooterCallback": function ( nRow, aaData, iStart, iEnd, aiDisplay ) {
 
                     var iTotal = 0;
-                    var i=0;
-                    for ( i=0; i<aaData.length; i++ ) {
+                    for ( var i=0 ; i<aaData.length ; i++ )
+                    {
                         iTotal += parseFloat(aaData[i][iTot].substr(8).replace(',','.'));
                     }
 
                     /* Modify the footer row to match what we want */
                     var nCells = $(nRow).find('th');
                     $(nCells[1]).html('&#8364; ' + String(GetRoundedFloat(iTotal)).replace('.',','));
-
-                    /* Modify Django management form info */
-                    /* FIXME should not be here this kind of logic computation */
-                    $('#' + block_obj.block_box_id + '-form-TOTAL_FORMS').val(iEnd-iStart);
                 }
             });
 
@@ -84,5 +77,5 @@ jQuery.UIBlockOrderReport = jQuery.UIBlockWithList.extend({
 
 });
 
-jQuery.BLOCKS.order_report = new jQuery.UIBlockOrderReport();
+jQuery.BLOCKS["order_report_intergas"] = new jQuery.UIBlockOrderInterGAS();
 
