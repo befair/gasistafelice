cda3b3ce83c138d7c2a902909e77370cd9802752
Luca Ferroni <luca@befair.it>
2013-01-21 09:48:10 +0100

    Added gas templatetags

---
+++ gasistafelice/gas/templatetags/gas_base.py
@@ -0,0 +1,10 @@
+
+from django import template
+from django.utils.translation import ugettext as _
+
+
+register = template.Library()
+
+@register.simple_tag
+def render_gas(gas):
+    return u"%s - %s (%s ordini = %s euro)" % (gas, gas.city, gas.orders.count(), gas.liquidity)
