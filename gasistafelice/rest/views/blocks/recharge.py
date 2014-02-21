befair/gasistafelice

commit f4b1ad6baa7fc4d34b3b0570ccdc0fe7b1bba0cc

Fixed Orly test#A -> this error is a print left in code!

gasistafelice/rest/views/blocks/recharge.py
@@ -115,7 +115,7 @@ class Block(BlockSSDataTables):
         for i,item in enumerate(querySet):
 
             form = formset[map_info[item.pk]['formset_index']]
-            print "item.balanceitem.balanceitem.balanceitem.balance %s" % ("%.2f" % round(item.balance, 2)).replace('.','€')
+            log.debug("item.balanceitem.balanceitem.balanceitem.balance %s" % ("%.2f" % round(item.balance, 2))).replace('.','€')
             records.append({
                'id' : item.pk,
                'gasmember' : item,

commit 2b8005fd301e20d3a1401321dc1486b533208c60

Fix bug #216
Fix error introduced in f4b1ad6b

gasistafelice/rest/views/blocks/recharge.py
@@ -115,7 +115,7 @@ class Block(BlockSSDataTables):
         for i,item in enumerate(querySet):
 
             form = formset[map_info[item.pk]['formset_index']]
-            log.debug("item.balanceitem.balanceitem.balanceitem.balance %s" % ("%.2f" % round(item.balance, 2))).replace('.','€')
+            log.debug("item.balanceitem.balanceitem.balanceitem.balance %s" % ("%.2f" % round(item.balance, 2)).replace('.','€'))
             records.append({
                'id' : item.pk,
                'gasmember' : item,