40abd21a4c388f092aa4e9f673688b88d00cdd6d
Luca Ferroni <luca@befair.it>
2015-06-12 01:01:37 +0200

    test data and view for gasmember 573 added to repository

--- gasistafelice/real_rest/views.py
+++ gasistafelice/real_rest/views.py
@@ -1,3 +1,4 @@
+#-*- encoding: utf-8 -*-
 #from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 #from django.core.exceptions import PermissionDenied
@@ -10,6 +11,15 @@ import serializers as my_serializers
 from gf.base.models import Person, Contact
 from gf.gas.models import GAS, GASMember
 from gf.supplier.models import Supplier
+
+def test573(request):
+    from django.http import HttpResponse
+    from django.conf import settings
+    import os.path
+    f = open(os.path.join(
+        os.path.dirname(settings.PROJECT_ROOT), 'test_data', 'gm573.json')
+    )
+    return HttpResponse(f.read())
 
 # REST API
 #--------------------------------------------------------------------------------
