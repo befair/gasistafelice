1e7d7ce82952179c23ce6a2c661edb31e8e5bd79
Luca Ferroni <luca@befair.it>
2015-06-16 08:54:24 +0200

    Cash panel Angular and controller stub working

--- gasistafelice/ui_ric1/static/ui_ric1/components/cash/cash.js
+++ gasistafelice/ui_ric1/static/ui_ric1/components/cash/cash.js
@@ -2,11 +2,13 @@ app.controller("CashController", function($http, $rootScope, $routeParams) {
 
     console.debug('CashController for gm=' + $rootScope.gm_id);
     this.transactions = [];
+    this.info = {};
     var THAT = this;
 
     $http.get($rootScope.absurl_api+'gasmember/' + $rootScope.gm_id+'/cash/?format=json')
     .success(function (data) {
-        THAT.transactions = data.transactions;
+        THAT.transactions = data.economic_movements;
+        THAT.info = data.cash_info;
     })
     .error(function (data) {
         THAT.transactions.push({
@@ -16,6 +18,11 @@ app.controller("CashController", function($http, $rootScope, $routeParams) {
             amount: -70.00,
             details: "ordine 38 REC - Alchemia"
         });
+        THAT.info = {
+            balance : 30.00,
+            total_basket : 20.00,
+            total_basket_to_be_delivered : 10.00
+        };
     });
 });
 
