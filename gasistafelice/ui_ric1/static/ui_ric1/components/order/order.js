3b6bc26f3ca310d680222a0c69f0cd926a732fb3
Luca Ferroni <luca@befair.it>
2015-06-17 16:25:40 +0200

    OrderController does GET of gasmember API by itself
    in the next commit app.js will be stripped of that GET

--- gasistafelice/ui_ric1/static/ui_ric1/components/order/order.js
+++ gasistafelice/ui_ric1/static/ui_ric1/components/order/order.js
@@ -1,21 +1,28 @@
-app.controller("OrderController", function($http, $rootScope, $routeParams, productManager) {
+app.controller("OrderController", function($http, $rootScope, productManager) {
 
     this.pm = productManager;
-    this.gm = $rootScope.gm;
     this.dataLoaded = true;
     
-    console.debug('OrderController for gm=' + this.gm.id);
-
     this.ordiniloaded = true;
     this.orderByField = ''; //?
     this.reverseSort = false; //?
 
-    if (this.gm.open_orders.length > 0) {
-        console.debug("Setting the default order catalog...");
-        this.pm.set_order_catalog(this.gm.open_orders[0]);
-    } else {
-        alert("Nessun ordine aperto per "+this.gm.gas.name);
-    }
+    $http.get($rootScope.absurl_api+'gasmember/' + $rootScope.gm_id+'/?format=json')
+    .success(function(data) {
 
+        $rootScope.gm = data;
+        var gm = $rootScope.gm;
+        console.debug('OrderController for gm=' + gm.id);
+
+        if (gm.open_orders.length > 0) {
+            console.debug("Setting the default order catalog...");
+            productManager.set_order_catalog(gm.open_orders[0], gm.basket);
+        } else {
+            alert("Nessun ordine aperto per " + gm.gas.name);
+        }
+        
+    }).error(function(data){
+        alert("http error get GAS member data");
+    });
 });
 
