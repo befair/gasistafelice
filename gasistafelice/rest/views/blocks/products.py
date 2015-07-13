46ca942c21fd487661f322f5521898b9e73c500a
Antonio Esposito <kobe@befair.it>
2015-07-13 22:53:42 +0200

    Fixed old default_settings references

--- gasistafelice/rest/views/blocks/products.py
+++ gasistafelice/rest/views/blocks/products.py
@@ -6,7 +6,7 @@ from django.utils.translation import ugettext as _, ugettext_lazy as _lazy
 #------------------------------------------------------------------------------#
 
 # COMMENT fero: Who cares about products? I think we only care about stocks!
-# COMMENT fero: See default_settings.py we have no products_block in there
+# COMMENT fero: See settings.py we have no products_block in there
 
 class Block(BlockWithList):
 
