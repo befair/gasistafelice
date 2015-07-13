ded54de4db2087bcb642e21770c476e3000f16c9
Antonio Esposito <kobe@befair.it>
2015-07-13 20:43:15 +0200

    WSGI callable now follows our conventions

--- gasistafelice/gf/wsgi.py
+++ gasistafelice/gf/wsgi.py
@@ -11,4 +11,4 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gf.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+app = get_wsgi_application()
