be305fbc502ad22133edfba14f28786f679d9aee
Antonio Esposito <kobe@befair.it>
2015-07-18 12:51:45 +0200

    Implemented backend autoreload on editing (dev-only)

--- gasistafelice/gf/wsgi.py
+++ gasistafelice/gf/wsgi.py
@@ -8,7 +8,20 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gf.settings")
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gf.settings")
 app = get_wsgi_application()
+
+if settings.ENV == 'dev':
+    from django.utils import autoreload
+    import uwsgi
+    from uwsgidecorators import timer
+
+    @timer(1)
+    def change_code_graceful_reload(sig):
+        if autoreload.code_changed():
+            uwsgi.reload()
