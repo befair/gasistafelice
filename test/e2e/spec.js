6cdf4dfe97f86a837d8da3fef656aad9c27b91ad
Michele Sorcinelli <mikefender@cryptolab.net>
2015-08-29 16:49:57 +0200

    E2E: improved test/e2e/spec.js

--- test/e2e/spec.js
+++ test/e2e/spec.js
@@ -17,8 +17,7 @@ describe('GF ng-app', function() {
       expect(browser.getTitle()).toEqual('Gasista Felice');
     });
 
-    it('should connect to the UI and get the user orders', function() {
-      browser.get('http://proxy:8080/');
+    it('should connect to the user order page', function() {
       // fill login form
       element(by.model('app.username')).sendKeys('01gas1');
       element(by.model('app.password')).sendKeys('des');
@@ -27,20 +26,27 @@ describe('GF ng-app', function() {
       // $$(selector) = element(by.css(selector))
       $$('#go').click();
 
-      // expect to be the user 01gas1
-      // on mobile the profile name is not available
+      // check current url
+      expect(browser.getLocationAbsUrl())
+        .toBe('/order/');
+
+      // check user displayed name
       if (index) {
         $$('.navbar-toggle').click();
-        expect(element(by.binding('person.display_name')).getText()).toBe("Gasista_01 DelGas_01");
+        expect(element(by.binding('person.display_name')).getText())
+          .toBe("Gasista_01 DelGas_01");
         $$('.navbar-toggle').click();
       }
       else
-        expect(element(by.binding('person.display_name')).getText()).toBe("Gasista_01 DelGas_01");
+        expect(element(by.binding('person.display_name')).getText())
+          .toBe("Gasista_01 DelGas_01");
     });
 
     it('should increment/decrement the price/qty when "+/-" are clicked', function () {
-      // get the first item in the table
-      var item = element.all(by.repeater('product in order.pm.products')).get(1);
+      // get the second item in the table
+      var item = element.all(
+                   by.repeater('product in order.pm.products')
+                 ).get(1);
 
       // click 20 time on '+'
       for (var i=0; i < 20; i++)
@@ -51,27 +57,37 @@ describe('GF ng-app', function() {
         item.$$('.glyphicon-minus').click();
 
       // qty should be 10
-      expect(item.element(by.model('product.quantity')).getAttribute('value')).toBe('10');
+      expect(item.element(by.model('product.quantity'))
+        .getAttribute('value'))
+          .toBe('10');
 
-      // product.quantity * product.price is used to output the total price
-      // so we can get that with by.binding('product.quantity')
-      // does the code need revision? the bindings maybe shouldn't make operations
-      expect(item.element(by.binding('product.quantity')).getText()).toBe('€ 250,00');
+      // price should be 250 euros
+      expect(item.element(by.binding('product.quantity'))
+        .getText())
+          .toBe('€ 250,00');
     });
 
     it('should never decrement the price/qty under 0', function () {
-      var item = element.all(by.repeater('product in order.pm.products')).get(1);
+      var item = element.all(
+                   by.repeater('product in order.pm.products')
+                 ).get(1);
 
       for (var i=0; i < 20; i++)
         item.$$('.glyphicon-minus').click();
 
-      expect(item.element(by.model('product.quantity')).getAttribute('value')).toBe('0');
+      expect(item.element(by.model('product.quantity'))
+        .getAttribute('value'))
+          .toBe('0');
 
-      expect(item.element(by.binding('product.quantity')).getText()).toBe('€ 0,00');
+      expect(item.element(by.binding('product.quantity'))
+        .getText())
+          .toBe('€ 0,00');
     });
 
     it('should add a product to the basket', function() {
-      var item = element.all(by.repeater('product in order.pm.products')).get(2);
+      var item = element.all(
+                   by.repeater('product in order.pm.products')
+                 ).get(2);
 
       // set the quantity to 3
       item.element(by.model('product.quantity')).clear();
@@ -87,21 +103,36 @@ describe('GF ng-app', function() {
       browser.setLocation('basket');
 
       // get the first order
-      item = element.all(by.repeater('item in basket.open_ordered_products')).get(0);
+      item = element.all(
+               by.repeater('item in basket.open_ordered_products')
+             ).get(0);
 
       // get all the column from the first order
       var columns = item.$$('td');
 
       // expects to have 8 columns (counting the hidden ones)
-      expect(columns.count()).toBe(8);
+      expect(columns.count())
+        .toBe(8);
 
       // check the fields
-      expect(columns.get(0).getText()).toBe('Ord. 59');
-      expect(columns.get(1).getText()).toBe('Fornitore 01');
-      expect(columns.get(2).getText()).toBe('Scarpe Uomo (paio)');
-      expect(columns.get(3).getText()).toBe('€ 20,00');
-      expect(item.element(by.model('item.quantity')).getAttribute('value')).toBe('3');
-      expect(columns.get(6).getText()).toBe('€ 60,00');
+      expect(columns.get(0)
+        .getText())
+          .toBe('Ord. 59');
+      expect(columns.get(1)
+        .getText())
+          .toBe('Fornitore 01');
+      expect(columns.get(2)
+        .getText())
+          .toBe('Scarpe Uomo (paio)');
+      expect(columns.get(3)
+        .getText())
+          .toBe('€ 20,00');
+      expect(item.element(by.model('item.quantity'))
+        .getAttribute('value'))
+          .toBe('3');
+      expect(columns.get(6)
+        .getText())
+          .toBe('€ 60,00');
     });
 
     it('should logout', function() {
